584f78cc613d2cdc54bdac674211bfcb9b840b55
torvic <130111@unsaac.edu.pe>
2022-03-23 21:06:22 -0500

    Add CategoryService and use it in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,5 @@
-import entities.Category;
-import entities.Option;
-import entities.Question;
-import services.CategoryServiceImpl;
-import services.OptionServiceImpl;
-import services.QuestionServiceImpl;
+import entities.*;
+import services.*;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -24,14 +20,53 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
 
-        System.out.println("Hola" + name);
+        System.out.println("Hola " + name + ", iniciemos el juego :)");
+        System.out.println("Presione 0 en cualquier ronda para poder retirarse con el premio acumulado");
+        System.out.println();
+
+        // app
+        boolean aux = true;
+        int roundNumber = 1;
+        int score = 0;
+
 
         Question question = questionService.getQuestionRandomByCategoryId("c1");
         System.out.println(question.getStatement());
+        System.out.println("Por favor ingrese un numero de las siguientes opciones: ");
 
-        for (Option option: optionService.getOptionsById("q1")) {
+        for (Option option: optionService.getOptionsByQuestionId(question.getId())) {
             System.out.println(option.getAlternative() + ") " + option.getDetail());
         }
+
+        System.out.println("Dificultad: " + categoryService.getCategoryById("c1").getName());
+        System.out.print("Respuesta: ");
+        int answer = Integer.parseInt(scanner.nextLine());
+        if (optionService.getOptionByQuestionIdAndAlternative(question.getId(), answer).getCorrect()) {
+            // create accumulated
+            AccumulatedServiceImpl accumulatedService = new AccumulatedServiceImpl();
+            Accumulated accumulated = accumulatedService.getOrCreateAccumulated(new Accumulated("a1", "u1", false));
+            // create round entity
+            RoundServiceImpl roundService = new RoundServiceImpl();
+            Round round = roundService.createRound(new Round(question.getId(), accumulated.getId()));
+
+            System.out.println("acumulado");
+            System.out.println(accumulated.getId() + accumulated.getPlayerId() + accumulated.getComplete());
+
+            System.out.println("ronda");
+            System.out.println(round.getQuestionId() + round.getAccumulatedId());
+
+            // calculated score by accumulated id
+            score = score + categoryService.getCategoryById("c1").getScore();
+            System.out.println("score: " + score);
+
+
+            System.out.println("Felicidades, pasas a la siguiente ronda");
+            //
+        } else {
+            System.out.println("Suerte en la proxima");
+            // update accumulated to isComplete true
+        }
+
 
     }
 }
--- src/main/java/services/CategoryServiceImpl.java
+++ src/main/java/services/CategoryServiceImpl.java
@@ -2,8 +2,10 @@ package services;
 
 import entities.Category;
 import entities.Option;
+import entities.Question;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class CategoryServiceImpl implements CategoryService{
 
@@ -11,6 +13,22 @@ public class CategoryServiceImpl implements CategoryService{
 
     public void createCategory(Category category) {
         categoryDB.add(category);
+    }
+
+    public Category getCategoryById(String categoryId) {
+        ArrayList<Category> filteredList;
+
+        filteredList = (ArrayList<Category>) categoryDB.stream()
+                .filter(category -> category.getId().equals(categoryId))
+                .collect(Collectors.toList());
+
+        return filteredList.get(0);
+    }
+
+    public Category getCategoryByQuestionId(String questionId) {
+        QuestionServiceImpl questionService = new QuestionServiceImpl();
+        Question question = questionService.getQuestionById(questionId);
+        return getCategoryById(question.getCategoryId());
     }
 
     public void makeMigrations() {
